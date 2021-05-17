af722527a232b26693fc231f518b70478f73088b
Annemarie Wisbar <a.wisbar@gmail.com>
2021-05-17 14:43:10 +0200

    Add PropTypes to PlayerForm

--- src/stories/PlayerForm.js
+++ src/stories/PlayerForm.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import PropTypes from 'prop-types'
+
+PlayerForm.propTypes = {
+  onSubmit: PropTypes.func,
+}
 
 export default function PlayerForm({ onSubmit }) {
   return (
     <form onSubmit={handleSubmit}>
       <label>
         Player name
-        <input name="name" placeholder="Player name" />
+        <input name="name" type="text" placeholder="Player name" />
       </label>
     </form>
   )
