7437233dfe9261a57da95cf2971661dbdee9da80
Annemarie Wisbar <a.wisbar@gmail.com>
2021-05-17 14:57:35 +0200

    Refactor PlayerForm stories

--- src/stories/PlayerForm.stories.js
+++ src/stories/PlayerForm.stories.js
@@ -4,14 +4,6 @@ import PlayerForm from './PlayerForm'
 export default {
   title: 'PlayerForm',
   component: PlayerForm,
-  argTypes: { onSubmit: { action: 'onSubmit' } },
 }
 
-//👇 We create a “template” of how args map to rendering
-const Template = args => <PlayerForm {...args} />
-
-export const DefaultForm = Template.bind({})
-
-DefaultForm.args = {
-  /*👇 The args you need here will depend on your component */
-}
+export const DefaultForm = args => <PlayerForm {...args} />
