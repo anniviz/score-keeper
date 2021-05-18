f490fc2f653463e21cb5da45326794ad3e1f6336
Annemarie Wisbar <a.wisbar@gmail.com>
2021-05-18 11:45:32 +0200

    Add Player Component

---
+++ src/Player.js
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types'
+import './Player.css'
+import Button from './Button'
+
+Button.propTypes = {
+  name: PropTypes.string,
+  score: PropTypes.number,
+  onMinusClick: PropTypes.func.isRequired,
+  onPlusClick: PropTypes.func.isRequired,
+}
+
+export default function Player({ name, score, onMinusClick, onPlusClick }) {
+  return (
+    <div className="Player">
+      <span>{name}</span>
+      <Button onClick={onMinusClick}>-</Button>
+      <span className="Player__score">{score}</span>
+      <Button onClick={onPlusClick}>+</Button>
+    </div>
+  )
+}
