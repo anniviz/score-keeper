78f676d36d43f8c68a667b2666bccb5074f9cf2d
Annemarie Wisbar <a.wisbar@gmail.com>
2021-05-18 12:08:05 +0200

    Add Components to App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { useState } from 'react'
 import './App.css'
+import Button from './Button'
+import Player from './Player'
+import PlayerForm from './PlayerForm'
 
 function App() {
-  return <div className="App"></div>
+  const [playerArray, setPlayerArray] = useState([
+    { name: 'Anni', score: 20 },
+    { name: 'Lucia', score: 30 },
+  ])
+
+  return (
+    <div className="App">
+      {playerArray.map(player => (
+        <Player key={player.name} name={player.name} score={player.score} />
+      ))}
+      <Button>Reset Scores</Button>
+      <Button onClick={handleResetAllClick}>Reset All</Button>
+      <PlayerForm></PlayerForm>
+    </div>
+  )
+
+  function handleResetAllClick() {
+    setPlayerArray([])
+  }
 }
 
 export default App
